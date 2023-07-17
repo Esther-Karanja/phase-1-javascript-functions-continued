4992cf4357c1bedb8be059b8671b136398b90ac3
Esther <est.karanja@gmail.com>
2023-07-17 16:28:06 -0700

    completed Functions: Continued lab

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// code your solution here
+
+function saturdayFun(activity){
+ if(activity===undefined ){
+   return "This Saturday, I want to roller-skate!"
+    }
+    return (`This Saturday, I want to ${activity}!`)
+
+}console.log(saturdayFun());
+
+function mondayWork(activity){
+    if(activity===undefined ){
+       return "This Monday, I will go to the office."
+    }
+   return (`This Monday, I will ${activity}.`)
+   
+}console.log(mondayWork());
+
+
+function wrapAdjective(initializer="||"){
+    return function(msg="a dedicated programmer"){
+        return `You are ${initializer}${msg}${initializer}!`
+    }
+ }
